a7bd0df8092b14f4377b9d6aec1689fe43d5bc64
Nicolas Mitchell <ncls.mitchell@gmail.com>
2016-05-15 01:11:07 +0200

    update useful_functions

--- useful_functions.js
+++ useful_functions.js
@@ -2,9 +2,6 @@
 TAGING
 ************************/
 
-//Select page <title>
-var pageTitle = document.getElementsByTagName('title')[0].innerHTML;
-
 //Convert string to string without spaces or special characters
 function toAtString (string) {
   string = string.replace(/ /g, "_").toLowerCase();
@@ -20,7 +17,7 @@ function toAtString (string) {
 }
 
 //Clicks taging automation
-<a href="#" at-data-name="mylink" at-data-type="exit">My link</a>
+//Link example: <a href="#" at-data-name="mylink" at-data-type="exit">My link</a>
 
 if ($(this).attr("at-data-name") !== null) {
 
@@ -36,33 +33,12 @@ function eventHandler(e) {
     if (e.data.service == "facebook_like") { //facebook_like: class of the link
         tag.click.send({elem:this, name: "pageName", level2: "level2", type: "action", chapter1: "chapter1", chapter2: "chapter2", chapter3: "facebook"});
     }
-    if (e.data.service == "google_plusone") { //google_plusone: class of the link
-        tag.click.send({elem:this, name: "pageName", level2: "level2", type: "action", chapter1: "chapter1", chapter2: "chapter2", chapter3: "google_plus"});
-    }
-    if (e.data.service == "tweet") { //tweet: class of the link
-        tag.click.send({elem:this, name: "pageName", level2: "level2", type: "action", chapter1: "chapter1", chapter2: "chapter2", chapter3: "twitter"});
-    }
-}
-
-//Test if adblock enabled
-function getAttributes(url) {
-
-  var attr = document.getElementsByTagName("img");
-
-  for (var i = 0; i < attr.length; i++) {
-
-    var attrSrc = attr[i].getAttribute("src");
-
-    if attrSrc.match(url) != null {
-        console.log(attrSrc);
-    }
-  }
 }
 
 //Force referrer
 delete window.document.referrer;
 window.document.__defineGetter__('referrer', function () {
-  return "facebook.com";
+  return "newReferrer.com";
 });
 
 
@@ -71,25 +47,15 @@ DASHBOARD APP
 ************************/
 
 //Convert Dashboard text into another, integrate between <script>...</script> in a text box
-<script>
-  function replaceElem(input, output, tag) {
-    var array = document.getElementsByTagName(tag);
-    var re = new RegExp(input);
+function replaceElem(input, output, tag) {
+  var array = document.getElementsByTagName(tag);
+  var re = new RegExp(input);
 
-    for (var i = 0; i < array.length; i++) {
-      array[i].innerHTML = array[i].innerHTML.replace(re, output);
-    }
+  for (var i = 0; i < array.length; i++) {
+    array[i].innerHTML = array[i].innerHTML.replace(re, output);
   }
+}
 
-  setTimeout(function(){
-    replaceElem("oldElem", "newElem", "span");
-  }, 1000);
-</script>
-
-/************************
-EXCEL - POWER QUERY
-************************/
-
-//Excel functions
-table = Excel.CurrentWorkbook(){[Name="tableName"]}[Content],
-date = table.Column(Table, "columnName"),
+setTimeout(function(){
+  replaceElem("oldElem", "newElem", "span");
+}, 1000);
